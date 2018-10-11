8b75239242974781453be7d54351cc74c776b2c1
Paul Keeler <40744936+hpaulkeeler@users.noreply.github.com>
2018-10-11 23:20:04 +0200

    Update PoissonDisk.py

--- PoissonDisk/PoissonDisk.py
+++ PoissonDisk/PoissonDisk.py
@@ -6,7 +6,7 @@ import scipy.stats
 import matplotlib.pyplot as plt
 
 #Simulation window parameters
-r=1;
+r=1;  #radius of disk
 xx0=0; yy0=0; #centre of disk
 
 areaTotal=np.pi*r**2; #area of disk
