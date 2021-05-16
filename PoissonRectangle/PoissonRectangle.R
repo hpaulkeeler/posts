38a4355ffda1514dad041fbc57208a8c563596db
Paul Keeler <hpkeeler@gmail.com>
2021-05-16 17:23:51 +1000

    Changed xDelta to yDelta

--- PoissonRectangle/PoissonRectangle.R
+++ PoissonRectangle/PoissonRectangle.R
@@ -17,7 +17,7 @@ lambda=100; #intensity (ie mean density) of the Poisson process
 #Simulate a Poisson point process
 numbPoints=rpois(1,areaTotal*lambda);#Poisson number of points
 xx=xDelta*runif(numbPoints)+xMin;#x coordinates of Poisson points
-yy=xDelta*runif(numbPoints)+yMin;#y coordinates of Poisson points
+yy=yDelta*runif(numbPoints)+yMin;#y coordinates of Poisson points
 
 #Plotting
 par(pty="s")
--- PoissonRectangle/PoissonRectangle.m
+++ PoissonRectangle/PoissonRectangle.m
@@ -17,7 +17,7 @@ lambda=100; %intensity (ie mean density) of the Poisson process
 %Simulate Poisson point process
 numbPoints=poissrnd(areaTotal*lambda);%Poisson number of points
 xx=xDelta*(rand(numbPoints,1))+xMin;%x coordinates of Poisson points
-yy=xDelta*(rand(numbPoints,1))+yMin;%y coordinates of Poisson points
+yy=yDelta*(rand(numbPoints,1))+yMin;%y coordinates of Poisson points
  
 %Plotting
 scatter(xx,yy);
